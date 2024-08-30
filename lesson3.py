14e410e6c32237b14cce46bb93053ce3016e810b
natashaseroff <natashaseroff@mail.ru>
2024-08-30 20:19:09 +0200

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,5 @@
+homework = 12
+hours = 1.5
+course = 'Python'
+time = (hours / homework)
+print('Курс:' , course , ', всего задач: ', homework, ', затрачено часов: ' , hours , ', среднее время выполнения', time)
